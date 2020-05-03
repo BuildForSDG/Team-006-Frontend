c0ac59ce30747c4af97a7de68c235a10d9deb420
unknown <arinzeokorji@yahoo.com>
2020-05-03 09:06:14 +0100

    Fix: Corrected some more errors given by codacy

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -65,6 +65,4 @@
 import 'zone.js/dist/zone'; // Included with Angular CLI.
 
 
-/***************************************************************************************************
- * APPLICATION IMPORTS
- */
+// APPLICATION IMPORTS
