c2c1209e8f594efb03da32014d2d42d38872c889
unknown <arinzeokorji@yahoo.com>
2020-05-03 01:47:25 +0100

    Fix: Fixed some of the errors caught by codacy

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -10,7 +10,7 @@ describe('AppComponent', () => {
       ],
       declarations: [
         AppComponent
-      ],
+      ]
     }).compileComponents();
   }));
 
@@ -20,7 +20,7 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'Andela-SDG-team006'`, () => {
+  it('should have as title "Andela-SDG-team006"', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app.title).toEqual('Andela-SDG-team006');
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,10 +1,10 @@
 // This file can be replaced during build by using the `fileReplacements` array.
 // `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
-
-export const environment = {
+const environment = {
   production: false
 };
+export default environment;
 
 /*
  * For easier debugging in development mode, you can import the following file
