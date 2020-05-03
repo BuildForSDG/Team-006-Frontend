dec56c3743ee1ff904f53aac0c14843adf371f17
unknown <arinzeokorji@yahoo.com>
2020-05-03 02:02:30 +0100

    Fix: Fixed more errors found by codacy

--- src/main.ts
+++ src/main.ts
@@ -2,13 +2,13 @@ import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
-import environment from './environments/environment';
+// @ts-ignore
+import environment from './environments/environment.ts';
 
 if (environment.production) {
   enableProdMode();
 }
 
 platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => {
-    alert(err);
+  .catch((err) => {
   });
