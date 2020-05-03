843b4055ffdeac3a7cfb25791d8af40c3c0ca7b6
Dave <callremz@gmail.com>
2020-05-03 17:54:14 +0100

    bug: airbnb/eslint-plugin-impor

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,23 +1,32 @@
 module.exports = {
-  env: {
-    browser: true,
-    es6: true,
-    node: true,
-    jest: true
+  parser: "@typescript-eslint/parser", // Specifies the ESLint parser
+  parserOptions: {
+    ecmaVersion: 2020, // Allows for the parsing of modern ECMAScript features
+    sourceType: "module" // Allows for the use of imports
   },
-  extends: ['airbnb-base',
-    'airbnb-angular'
-  ],
+
+  extends: [
+    "airbnb-base",
+    "airbnb-angular",
+    "plugin:@typescript-eslint/recommended", // Uses the recommended rules from the @typescript-eslint/eslint-plugin
+    "plugin:import/typescript",
+    "prettier/@typescript-eslint", // Uses eslint-config-prettier to disable ESLint rules from @typescript-eslint/eslint-plugin that would conflict with prettier
+    "plugin:prettier/recommended" // Enables eslint-plugin-prettier and eslint-config-prettier. This will display prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array. 
+  ],  
+ 
   globals: {
     Atomics: 'readonly',
     SharedArrayBuffer: 'readonly'
   },
-  parserOptions: {
-    ecmaVersion: 2018,
-    sourceType: 'module'
-  },
   rules: {
     'comma-dangle': ['error', 'never'],
-    'no-param-reassign': ['error', { props: false }]
+    'no-param-reassign': ['error', { props: false }],
+    'import/extensions': ['error', 'ignorePackages', {
+      js: 'never',
+      mjs: 'never',
+      jsx: 'never',
+      ts: 'never',
+      tsx: 'never',
+    }],
   }
 };
--- src/polyfills.ts
+++ src/polyfills.ts
@@ -69,5 +69,4 @@
  */
 import 'zone.js/dist/zone'; // Included with Angular CLI.
 
-
 // APPLICATION IMPORTS
