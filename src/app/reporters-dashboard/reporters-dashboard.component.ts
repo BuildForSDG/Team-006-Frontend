9c75eb742b45feec0beb8c6a3c862b6c5e328951
unknown <arinzeokorji@yahoo.com>
2020-05-07 16:48:46 +0100

    feat: tag selection configure

--- src/app/reporters-dashboard/reporters-dashboard.component.ts
+++ src/app/reporters-dashboard/reporters-dashboard.component.ts
@@ -6,7 +6,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./reporters-dashboard.component.scss']
 })
 export class ReportersDashboardComponent implements OnInit {
+  listOfTags = ['fire', 'accident', 'water'];
+
+  selectedTags: string[] = [];
+
   constructor() {}
 
   ngOnInit() {}
+
+  selectTag(tag: string) {
+    if (this.selectedTags.includes(tag)) {
+      return;
+    } else {
+      this.selectedTags.unshift(tag);
+      console.log(this.selectedTags);
+    }
+  }
+
+  removeTag(tag: string) {
+    this.selectedTags = this.selectedTags.filter((t) => {
+      return t !== tag;
+    });
+    console.log(this.selectedTags);
+  }
 }
