0e277ba3d06879460e5a347651234d4746ee06fb
unknown <arinzeokorji@yahoo.com>
2020-05-17 19:54:28 +0100

    [feature]: Create auth module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,11 @@ import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
+import { AuthModule } from './auth/auth.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, SharedModule, FontAwesomeModule],
+  imports: [BrowserModule, AppRoutingModule, SharedModule, FontAwesomeModule, AuthModule],
   providers: [],
   bootstrap: [AppComponent]
 })
